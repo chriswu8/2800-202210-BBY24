b7f3273980e70223ff81426b790c5ccf237a4936
Chris Wu <zezemint@gmail.com>
2022-05-14 10:23:26 -0700

    modified app.js and package.json

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { engine } = require('express/lib/application');
+const { engine } = require('express/lib/application'); // automatically generated
 const path = require('path'); //  provides a way of working with directories and file paths.
-
 const app = express();
+const mongoose = require('mongoose');
+
+// connect to db
+const db = mongoose.connect('mongodb://localhost:27017/cart')
+    .then(
+        function () { console.log("Connected to local MongoDB. Nice!"); },
+        err => { handleError(error) }
+    );
 
 // EJS setup
 app.set('views', path.join(__dirname, 'views'));
@@ -11,12 +18,12 @@ app.set('view engine', 'ejs');
 // set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('working');
 });
 
 // start server
 const port = 8000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Listening to port ' + port);
 });
