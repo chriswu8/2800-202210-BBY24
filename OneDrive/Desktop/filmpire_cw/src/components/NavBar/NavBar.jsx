fdeb275d9e3b685f35f039fe22ea4d76dcddbdd1
Chris Wu <zezemint@gmail.com>
2022-06-14 14:47:22 -0700

    styled the Toolbar

--- OneDrive/Desktop/filmpire_cw/src/components/NavBar/NavBar.jsx
+++ OneDrive/Desktop/filmpire_cw/src/components/NavBar/NavBar.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { AppBar, Avatar, Button, Drawer, IconButton, Toolbar } from '@mui/material';
 import { AccountCircle, Brightness4, Brightness7, Menu } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
 const NavBar = () => {
-  console.log('movie info');
+  const classes = useStyles();
+
   return (
     <>
-      <AppBar>
-        <h1>Nav!</h1>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          <h1>Nav!!!</h1>
+        </Toolbar>
       </AppBar>
     </>
   );
---
+++ OneDrive/Desktop/filmpire_cw/src/components/NavBar/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  toolbar: {
+    height: '88px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginLeft: '240px',
+    color: '#eddbcd',
+    backgroundColor: '#192231',
+  },
+
+}));
