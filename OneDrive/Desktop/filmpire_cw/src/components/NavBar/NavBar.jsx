9410c13624a483d7a6fea7609cde50a52afb1167
Chris Wu <zezemint@gmail.com>
2022-06-14 14:52:18 -0700

    imported the mui utility useMediaQuery to check for mobile divices

--- OneDrive/Desktop/filmpire_cw/src/components/NavBar/NavBar.jsx
+++ OneDrive/Desktop/filmpire_cw/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { AppBar, Avatar, Button, Drawer, IconButton, Toolbar } from '@mui/material';
+import { AppBar, Avatar, Button, Drawer, IconButton, Toolbar, useMediaQuery } from '@mui/material';
 import { AccountCircle, Brightness4, Brightness7, Menu } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import useStyles from './styles';
 
 const NavBar = () => {
   const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
 
   return (
     <>
