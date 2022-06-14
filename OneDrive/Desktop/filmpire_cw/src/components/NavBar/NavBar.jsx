f7a637470dd3bcf91f807a36f3a1932249076747
Chris Wu <zezemint@gmail.com>
2022-06-14 14:28:18 -0700

    imported Link from react-router-dom

---
+++ OneDrive/Desktop/filmpire_cw/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppBar, Avatar, Button, Drawer, IconButton, Toolbar } from '@mui/material';
+import { AccountCircle, Brightness4, Brightness7, Menu } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  console.log('movie info');
+  return (
+    <div>NavBar</div>
+  );
+};
+
+export default NavBar;
